9d7cc5a3d8ea17c4b3b92a1e388db9dd346a0deb
Sivakornsir <141816219+Sivakornsir@users.noreply.github.com>
2023-12-22 09:39:42 +0000

    lab11_1

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,83 +1,51 @@
-#include<iostream>
-#include<string>
-#include<ctime>
-#include<cstdlib>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
 
 using namespace std;
-
-string cardNames[] = {"","A","2","3","4","5","6","7","8","9","10","J","Q","K"};
-int cardScores[] = {0,1,2,3,4,5,6,7,8,9,10,10,10,10};
-
-int drawCard(void){
-    return rand()%13+1;
-}
-
-int calScore(int x,int y,int z){
-    int score_x = cardScores[x];
-    int score_y = cardScores[y];
-    int score_z = cardScores[z];
-	return (score_x+score_y+score_z)%10;
-}
-
-int findYugiAction(int s){        
-        if(s == 9) return 2; 
-        else if(s < 6) return 1;
-        else{
-                if(rand()%100+1 <= 31) return 2;
-                else return 1;
+bool checkEnter(int& counter) {
+    if (cin.get() == '\n') {
+        counter++;
+        if (counter == 3) {
+            return true;
         }
+    }
+    
+    return false;
 }
 
-void checkWinner(int p, int y){
-        cout << "\n---------------------------------\n";
-        if(p == y) 
-            cout <<   "|             Draw!!!           |"; 
-        else if(p > y) 
-            cout <<   "|         Player wins!!!        |"; 
-        else 
-            cout <<   "|          Yugi wins!!!         |"; 
-        cout << "\n---------------------------------\n";
+char getRandomGrade() {
+    const char grades[] = {'A', 'B', 'C', 'D', 'F', 'W'};
+    return grades[rand() % sizeof(grades)];
 }
 
-int main(){        
-        srand(time(0));
+char getRandomPD() {
+    const char PD[] = {'+',' '};
+    return PD[rand() % sizeof(PD)];
+}
 
-        int playerScore, yugiScore, playerAction, yugiAction;
-        int playerCards[3] = {drawCard(), drawCard(), 0};
-        int yugiCards[3] = {drawCard(), drawCard(), 0};
-        
-        cout << "---------ORE NO TURN, DRAW!!!------------\n";
-        cout << "Your cards: " << cardNames[playerCards[0]] << " " << cardNames[playerCards[1]] << "\n";
-        playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
-        cout << "Your score: " << playerScore << "\n";
-        do{
-                cout << "(1) Destiny draw (2) Stay, SELECT: ";
-                cin >> playerAction;
-        }while(playerAction != 1 && playerAction != 2);
-        if(playerAction == 1){
-                playerCards[2] = drawCard();
-                playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
-                cout << "Player draws the 3rd card!!!" << "\n";
-                cout << "Your 3rd card: " << cardNames[playerCards[2]] << "\n";
-                cout << "Your new score: " << playerScore << "\n";
-        }
-        cout << "------------ Turn end -------------------\n\n";
+int main(){
+    cout << "Press Enter 3 times to reveal your future." << endl;
 
-		cout << "---------YUGI'S TURN, DRAW!!!------------\n";
-        cout << "Yugi's cards: " << cardNames[yugiCards[0]] << " " << cardNames[yugiCards[1]] << "\n";
-        yugiScore = calScore(yugiCards[0],yugiCards[1],yugiCards[2]);
-        cout << "Yugi's score: " << yugiScore << "\n";
-        yugiAction = findYugiAction(yugiScore);
-        if(yugiAction == 1){
-                yugiCards[2] = drawCard();
-                yugiScore = calScore(yugiCards[0],yugiCards[1],yugiCards[2]);
-                cout << "Yugi draws the 3rd card!!!\n";
-                cout << "Yugi's 3rd card: " << cardNames[yugiCards[2]] << "\n";
-                cout << "Yugi's new score: " << yugiScore << "\n";
-        }
-        cout << "------------ Turn end -------------------\n";
+    int enterCounter = 0;
+    char grade;
 
+    srand(time(0));
 
+    while (!checkEnter(enterCounter)) {
+    }
+char graderand = getRandomGrade();
+string PD;
+if (graderand == 'B'||graderand=='C'||graderand=='D'){
+    PD = getRandomPD();
+    if(PD == "+"){
+        PD=PD+' ';
+    }
+}else {
+    PD = ' ';
+}
+    cout << "You will get " << graderand << PD <<"in this 261102."<< endl;
 
-	checkWinner(playerScore,yugiScore);
+    return 0;
 }
